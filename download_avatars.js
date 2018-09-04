4ac4cc3eb841cc6da4acec0c7889ea8180988e82
Silvia Bon <bon.silvia@gmail.com>
2018-09-04 01:50:17 +0000

    printing avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,24 +1,30 @@
+var request = require('request');
 var secrets = require('./secrets');
 
-var request = require('request');
-
 console.log('Welcome to the GitHub Avatar Downloader!');
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': secrets.GITHUB_TOKEN
+      'Authorization': "token " + secrets.GITHUB_TOKEN
     }
   };
   request(options, function(err, res, body) {
-    cb(err, body);
+    var contributors = JSON.parse(body);
+    cb(err, contributors);
   });
 }
 
+
+
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  //console.log("Result:", result);
+  result.forEach(function(contrib){
+    console.log(contrib.avatar_url);
+  })
 });
-
